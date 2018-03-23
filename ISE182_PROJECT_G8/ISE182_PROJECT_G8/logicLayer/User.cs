fc7e9471c1eb1995425b1b8270b587e4ad2e12a1
chendoy <chendoy@post.bgu.ac.il>
2018-03-23 18:01:09 +0300

    implementing user+test commit

--- ISE182_PROJECT_G8/ISE182_PROJECT_G8/logicLayer/User.cs
+++ ISE182_PROJECT_G8/ISE182_PROJECT_G8/logicLayer/User.cs
@@ -8,6 +8,15 @@ namespace logicLayer
 {
     class User
     {
-      
+        private String nickname;
+        private String status;
+
+        public User(String nickname, String status)
+        {
+            this.nickname = nickname;
+            this.status = status;
+        }
+
+        public void logout()
     }
 }
