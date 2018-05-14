2d19f27bd3f1d8a269eb46e0ada00b6af24d3801
Naor Kolet <Naork94@gmail.com>
2018-05-14 16:04:26 +0300

    LoginObserver

---
+++ ISE182_PROJECT_G8/ISE182_PROJECT_G8/presentationLayer/LoginObserver.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISE182_PROJECT_G8.presentationLayer
+{
+    public class LoginObserver : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string username;
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+
+        private string groupID;
+        public string GroupID
+        {
+            get
+            {
+                return groupID;
+            }
+            set
+            {
+                groupID = value;
+                OnPropertyChanged("GroupID");
+            }
+        }
+
+        private string errorMessage;
+        public string ErrorMessage
+        {
+            get
+            {
+                return errorMessage;
+            }
+            set
+            {
+                errorMessage = value;
+                OnPropertyChanged("ErrorMessage");
+            }
+        }
+    }
+}
