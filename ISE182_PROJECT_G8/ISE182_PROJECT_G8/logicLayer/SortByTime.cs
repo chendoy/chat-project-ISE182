f2f84170af423d82c21ec24ead94ba1ed7a4a2c1
sundersb <sundersb@post.bgu.ac.il>
2018-05-20 20:29:57 +0200

    Added Abstract class and sort by time method

---
+++ ISE182_PROJECT_G8/ISE182_PROJECT_G8/logicLayer/SortByTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISE182_PROJECT_G8.logicLayer
+{
+    class SortByTime : Sorter
+    {
+        private  List<Message> sorttime;
+        public SortByTime(List<Message> sorttime)
+        {
+            this.sorttime = sorttime;
+        }
+        public override List<Message> Sort(List<Message> messages)
+        {
+            List<Message> sorted = new List<Message>();
+            sorted = sorttime.OrderBy(o => o.getTime()).ToList();
+            return sorted;
+        }
+    }
+}
---
+++ ISE182_PROJECT_G8/ISE182_PROJECT_G8/logicLayer/Sorter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISE182_PROJECT_G8.logicLayer
+{
+    abstract class Sorter
+    {
+        private List<Message> list;
+        public Sorter(List<Message> list)
+        {
+            this.list = list;
+        }
+        abstract public List<Message> Sort(List<Message> messages);
+    }
+}
