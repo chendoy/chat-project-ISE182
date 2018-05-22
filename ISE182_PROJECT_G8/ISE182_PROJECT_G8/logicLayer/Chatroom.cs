ae8d38d7ba9a2dcc9a06eca955a09a505c353152
Naorko <Naorko@post.bgu.ac.il>
2018-05-22 23:53:41 +0300

    finito

--- ISE182_PROJECT_G8/ISE182_PROJECT_G8/logicLayer/Chatroom.cs
+++ ISE182_PROJECT_G8/ISE182_PROJECT_G8/logicLayer/Chatroom.cs
@@ -18,7 +18,7 @@ namespace ISE182_PROJECT_G8.logicLayer
      */
     public class Chatroom
     {
-        public String _url = "http://ise172.ise.bgu.ac.il"; // "http://localhost/"; //  
+        private String _url = "http://ise172.ise.bgu.ac.il"; // "http://localhost/"; //  
         private int port = 80;
         private User loggedInUser;
         private List<User> userList;  //in RAM, retreived from persistant layer//
