5374f2bb8b8e8abcd3cf741c888238c16a9655ac
Naorko <Naorko@post.bgu.ac.il>
2018-06-07 13:26:31 +0300

    Start DAL

---
+++ ISE182_PROJECT_G8/ISE182_PROJECT_G8/dataAccessLayer/DBA.cs
@@ -0,0 +1,38 @@
+﻿using ISE182_PROJECT_G8.logicLayer;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISE182_PROJECT_G8.dataAccessLayer
+{
+    class DBA
+    {
+        private readonly string server_address = "ise172.ise.bgu.ac.il,1433\\DB_LAB";
+        private readonly string database_name = "MS3";
+        private readonly string user_name = "publicUser";
+        private readonly string password = "isANerd";
+    
+        private SqlConnection connection;
+
+        public DBA()
+        {
+            string connetion_string = null;
+            connetion_string = $"Data Source={server_address};Initial Catalog={database_name };User ID={user_name};Password={password}";
+            this.connection = new SqlConnection(connetion_string);
+        }
+
+        public ObservableCollection<Message> GetNewMessages(DateTime fromTime)
+        {
+            string selectFields = "*";
+            string fromTable = "Messages";
+            string dateToString = fromTime.ToString("yyyy'-'MM'-'dd HH':'mm':'ss'.'ms", System.Globalization.DateTimeFormatInfo.InvariantInfo);
+            string where = $"SendTime >= '{dateToString}'";
+            Query query = new Query(selectFields, fromTable, where);
+            return query.Excute<Message>(this.connection);
+        }
+    }
+}
